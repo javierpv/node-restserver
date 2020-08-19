185f0cc384267d219beca8e400d253ea7f176e41
Javier <jpadron@telefonica.net>
2020-08-19 10:32:49 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //============
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//============
+// Entorno
+//============
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//============
+// Base de datos
+//============
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev')
+    urlDB = 'mongodb://localhost:27017/cafe';
+else
+    urlDB = 'mongodb+srv://jpv:8CQaAEaFdbVW44vn@cluster0.vr8is.mongodb.net/cafe';
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,50 +1,21 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
+app.use(require('./routes/usuario'));
+
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario');
-});
-
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if(body.nombre === undefined){
-
-        res.status(400).json({
-            ok: false,
-            message: "El nombre es necesario"
-        });
-
-    }
-    else{
-        res.json({
-        persona: body
-    });
-    }
-    
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario');
-});
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(resp => { console.log('Base de datos ONLINE') })
+    .catch(err => { console.log("Error al conectar base de datos", err) });
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando en el puerto ${process.env.PORT}`);
